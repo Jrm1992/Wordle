7fb1732b6b6097346df4c3da1e74bdd2a23eef12
Jose Roberto Marques <marquesjoseroberto@proton.me>
2026-04-24 17:45:12 -0300

    feat(auth): add sign-out button in header (#27)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,29 @@
 import Logo from '../../../assets/Logo.svg';
-import { Container, Text } from './style';
+import { useAuth } from '../../hooks/useAuth';
+import { Container, LogoGroup, SignOutButton, Spacer, Text } from './style';
+
+import { SignOut } from 'phosphor-react-native';
 
 export default function Header() {
+  const { signOut } = useAuth();
+
   return (
     <Container>
-      <Logo />
-      <Text>ordle</Text>
+      <Spacer />
+      <LogoGroup>
+        <Logo />
+        <Text>ordle</Text>
+      </LogoGroup>
+      <SignOutButton
+        onPress={() => {
+          signOut();
+        }}
+        hitSlop={8}
+        accessibilityRole="button"
+        accessibilityLabel="Sign out"
+      >
+        <SignOut size={24} color="#ffffff" weight="regular" />
+      </SignOutButton>
     </Container>
   );
 }
